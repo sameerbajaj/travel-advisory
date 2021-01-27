db00a32943ec5bbf8dd88b1f224d124c063484a3
Sameer Bajaj <sameerbajaj24@gmail.com>
2021-01-26 17:33:00 -0800

    version_0

---
+++ travel-advisory.py
@@ -0,0 +1,31 @@
+import requests
+from urllib.request import urlopen
+
+
+url = "https://www.cdph.ca.gov/Programs/CID/DCDC/Pages/COVID-19/Travel-Advisory.aspx"
+
+page = urlopen(url)
+
+html_bytes = page.read()
+
+html = html_bytes.decode("utf-8")
+
+
+updated_at = html[113309:113332]
+
+
+'Updated January 6, 2021' == updated_at
+
+
+report = {}
+report["value1"] = updated_at
+report["value2"] = 'second'
+report["value3"] = 'first'
+
+
+ifttt_key = os.environ["IFTTT_KEY"]
+
+
+ifttt_url = f"https://maker.ifttt.com/trigger/Scraper/with/key/{ifttt_key}"
+
+requests.post(ifttt_url,data=report)
